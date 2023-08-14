b9dd5daefce46d87e5dfb7f2124fa569b5fcf7cd
Khansaaufarafiqi <kansaaurafiki12@gmail.com>
2023-08-14 22:34:15 +0700

    menambahkan ,calender,kanban,colorpicer,costomers

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -10,7 +10,7 @@ const Ecommerce = () => {
       <div className="mt-12 ">
         <div className="flex flex-wrap lg:flex-nowrap justify-center ">
           <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg
-           h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center ">
+           h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover  bg-right">
               <div className="flex justify-between items-center">
                 <div>
                   <p className="font-bold text-gray-400">Earnings</p>
@@ -106,6 +106,20 @@ const Ecommerce = () => {
                           color="blue"                     
                         />
                      </div>
+                     <div className="mt-10">
+                        <Button 
+                          color="white"
+                          bgColor="blue"
+                          text="Download Report"
+                          borderRadius="10px"
+                        >
+                        </Button>
+                     </div>
+                </div>
+                <div>
+                  <Stacked width="320px" height="360px">
+
+                  </Stacked>
                 </div>
               </div>
             </div>
